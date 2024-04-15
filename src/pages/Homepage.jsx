a84ec421a00aebe960160596452b14a689416ce2
Alcides Ojeda <alcides.ojeda@mail.com>
2024-04-15 10:50:34 +0200

    add navlink

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,13 @@
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-    return (
-        <div>
-            Home
-        </div>
-    )
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+      {/* <a href="/pricing">Pricing</a> triggers a reload from server */}
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
