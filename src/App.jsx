b8cdfecfc8ea41e518ae586206f5063de96877c8
Alcides Ojeda <alcides.ojeda@mail.com>
2024-04-15 11:26:09 +0200

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFoun";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         {/* default route if nothing matches */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
